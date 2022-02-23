146a2230de77c306f530b97234ac24e0dcdd7c82
henriquekikuchi <rickikuchi@gmail.com>
2022-02-23 13:41:49 -0300

    corrigindo metodo de deletar comentario

--- src/main/java/br/com/kikuchi/henrique/todolistv3/repository/CommentRepository.java
+++ src/main/java/br/com/kikuchi/henrique/todolistv3/repository/CommentRepository.java
@@ -2,13 +2,17 @@ package br.com.kikuchi.henrique.todolistv3.repository;
 
 import br.com.kikuchi.henrique.todolistv3.model.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
 
-    public Optional<List<Comment>> findAllByTaskId(Integer taskId);
-    public Optional<Comment> findByIdAndTaskId(Integer id, Integer taskId);
-    public void deleteByIdAndTaskId(Integer id, Integer taskId);
+    Optional<List<Comment>> findAllByTaskId(Integer taskId);
+    Optional<Comment> findByIdAndTaskId(Integer id, Integer taskId);
+    @Transactional
+    @Modifying
+    void deleteByIdAndTaskId(Integer id, Integer taskId);
 }
